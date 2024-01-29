c70b846964b260ca44524335b1f4763662c053ae
IPESH SAPKOTA KR <230587@jc-21.jp>
2024-01-29 16:35:49 +0900

    .

--- Player.cpp
+++ Player.cpp
@@ -2,7 +2,9 @@
 #include"Engine/Model.h"
 #include"Engine/Input.h"
 #include"Engine/Camera.h"
-
+namespace {
+	const float player_movespeed = 0.015f;
+}
 Player::Player(GameObject* parent)
 	:GameObject(parent,"Player"),hplayer(-1)
 {
@@ -22,20 +24,41 @@ void Player::Initialize()
 
 void Player::Update()
 {
+	/*enum Dir {
+		UP,LEFT,DOWN,RIGHT,NONE,
+	};
+	int moveDir = Dir::NONE;*/
+
+	XMVECTOR vfront = { 0,0,1,0 };
+	XMVECTOR move{ 0,0,1,0 };
 	
 	if (Input::IsKey(DIK_A)) {
-		transform_.position_.x -= 0.15;
-
+		move = XMVECTOR{ -1,0,0,0 };
+		//moveDir = Dir::LEFT;
 	}
 	if (Input::IsKey(DIK_D)) {
-		transform_.position_.x += 0.15;
+		move = XMVECTOR{ 1,0,1,0 };
+	//	moveDir = Dir::RIGHT;
 	}
 	if (Input::IsKey(DIK_W)) {
-		transform_.position_.z += 0.15;
+		move = XMVECTOR{ 0,0,1,0 };
+	//	moveDir = Dir::UP;
 	}
 	if (Input::IsKey(DIK_S)) {
-		transform_.position_.z -= 0.15;
+		move = XMVECTOR{ 0,0,-1,0 };
+		//moveDir = Dir::DOWN;
 	}
+	XMVECTOR pos = XMLoadFloat3(&(transform_.position_));
+	pos = pos + speed_ * move;
+	XMStoreFloat3(&(transform_.position_), pos);
+	XMVECTOR vdot=XMVector3Dot(vfront, move);
+	float angle = acos(XMVectorGetX(vdot));
+		transform_.rotate_.y=XMConvertToDegrees(angle);
+		XMVECTOR vcross = XMVector3Cross(vfront, move);
+		if (XMVectorGetY(vcross) < 0) {
+			angle *= -1;
+		}
+		transform_.rotate_.y = XMConvertToDegrees(angle);
 }
 
 void Player::Draw()
