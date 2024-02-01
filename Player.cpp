6d3cc04472efd37bb1709766b5e5314763525dc0
IPESH SAPKOTA KR <230587@jc-21.jp>
2024-02-01 13:55:23 +0900

    ..

--- Player.cpp
+++ Player.cpp
@@ -2,8 +2,9 @@
 #include"Engine/Model.h"
 #include"Engine/Input.h"
 #include"Engine/Camera.h"
+#include"Engine//Debug.h"
 namespace {
-	const float player_movespeed = 0.05f;
+	const float player_movespeed = 1.0f;
 }
 Player::Player(GameObject* parent)
 	:GameObject(parent,"Player"),hplayer(-1),speed_(player_movespeed)
@@ -29,30 +30,39 @@ void Player::Update()
 	};
 	int moveDir = Dir::NONE;*/
 
-	XMVECTOR vfront = { 0,0,1,0 };
-	XMVECTOR move{ 0,0,1,0 };
+	XMVECTOR vfront = { 0,0,1,0 };//direction
+	XMVECTOR move{ 0,0,0,0 };//movement vector
 	
-	if (Input::IsKey(DIK_A)) {
+	if (Input::IsKeyDown(DIK_A)) {
 		move = XMVECTOR{ -1,0,0,0 };
 		//moveDir = Dir::LEFT;
 	}
-	if (Input::IsKey(DIK_D)) {
+	if (Input::IsKeyDown(DIK_D)) {
 		move = XMVECTOR{ 1,0,0,0 };
 	//	moveDir = Dir::RIGHT;
 	}
-	if (Input::IsKey(DIK_W)) {
+	if (Input::IsKeyDown(DIK_W)) {
 		move = XMVECTOR{ 0,0,1,0 };
 	//	moveDir = Dir::UP;
 	}
-	if (Input::IsKey(DIK_S)) {
+	if (Input::IsKeyDown(DIK_S)) {
 		move = XMVECTOR{ 0,0,-1,0 };
 		//moveDir = Dir::DOWN;
 	}
 	XMVECTOR pos = XMLoadFloat3(&(transform_.position_));
 	pos = pos + speed_ * move;
+	Debug::Log("X,Z=");
+	Debug::Log(XMVectorGetX(pos));
+	Debug::Log(",");
+	Debug::Log(XMVectorGetZ(pos),true);
+
+
+	if(!XMVector3Equal(move,XMVectorZero())){
 	XMStoreFloat3(&(transform_.position_), pos);
 
 	XMVECTOR vdot=XMVector3Dot(vfront, move);
+	assert(XMVectorGetX(vdot) <= 1 && XMVectorGetX(vdot) >= -1);
+	
 	float angle = acos(XMVectorGetX(vdot));
 
 		XMVECTOR vcross = XMVector3Cross(vfront, move);
@@ -61,11 +71,12 @@ void Player::Update()
 		}
 		transform_.rotate_.y = XMConvertToDegrees(angle);
 }
-
+	}
 void Player::Draw()
 {
 	Model::SetTransform(hplayer, transform_);
 	Model::Draw(hplayer);
+
 }
 
 void Player::Release()
